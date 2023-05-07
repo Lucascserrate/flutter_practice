fa771957844cd844aee41e43daff9645f4eac54d
Lucas Camacho <Lucascserrate@gmail.com>
2023-05-07 02:16:34 -0400

    ADD SwiperComponent

--- lib/pages/swiper.dart
+++ lib/pages/swiper.dart
@@ -18,34 +18,30 @@ class _SwiperComponentState extends State<SwiperComponent> {
   ];
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: Scaffold(
-          body: Container(
-            decoration: const BoxDecoration(color: Color(0xff000005)),
-            child: Center(
-              child: SizedBox(
-                width: double.infinity,
-                height: 350.0,
-                child: Swiper(
-                  viewportFraction: 0.8,
-                  scale: 0.9,
-                  itemBuilder: (BuildContext context, int index) {
-                    return ClipRRect(
-                      borderRadius: BorderRadius.circular(8),
-                      child: Image.network(
-                        images[index],
-                        fit: BoxFit.cover,
-                      ),
-                    );
-                  },
-                  itemCount: images.length,
-                  pagination: const SwiperPagination(),
-                  control: const SwiperControl(),
+    return Container(
+      decoration: const BoxDecoration(color: Color(0xff000005)),
+      child: Center(
+        child: SizedBox(
+          width: double.infinity,
+          height: 350.0,
+          child: Swiper(
+            viewportFraction: 0.8,
+            scale: 0.9,
+            itemBuilder: (BuildContext context, int index) {
+              return ClipRRect(
+                borderRadius: BorderRadius.circular(8),
+                child: Image.network(
+                  images[index],
+                  fit: BoxFit.cover,
                 ),
-              ),
-            ),
+              );
+            },
+            itemCount: images.length,
+            pagination: const SwiperPagination(),
+            control: const SwiperControl(),
           ),
-        ));
+        ),
+      ),
+    );
   }
 }
